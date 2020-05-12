cfdc9be7e4b9dac4bc46b801a708ee4dc7f09361
Hugo Barros Camboim <git@hugobarros.com.br>
2020-05-11 21:20:02 -0300

    WIP

--- src/main/java/com/algaworks/algafoodapi/notificacao/NoticadorEmail.java
+++ src/main/java/com/algaworks/algafoodapi/notificacao/NoticadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafoodapi.notificacao;
 
 import com.algaworks.algafoodapi.domain.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +10,16 @@ import org.springframework.stereotype.Component;
 @TipoNotificador(NivelUrgencia.NORMAL)
 public class NoticadorEmail implements Notificador {
 
+    @Value("${notificador.email.host}")
+    private String host;
+
+    @Value("${notificador.email.porta}")
+    private Integer porta;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host);
+        System.out.println("Porta: " + porta);
         System.out.printf("Notificando %s através do email %s: %s", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
