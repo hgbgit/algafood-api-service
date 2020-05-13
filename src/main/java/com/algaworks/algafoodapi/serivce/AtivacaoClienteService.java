3f7148e7243dc9a18137119ad73ec36f4a0b6da8
Hugo Barros Camboim <git@hugobarros.com.br>
2020-05-12 22:03:05 -0300

    WIP

--- src/main/java/com/algaworks/algafoodapi/serivce/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafoodapi/serivce/AtivacaoClienteService.java
@@ -5,26 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 @Component
 public class AtivacaoClienteService {
 
     @Autowired
     ApplicationEventPublisher eventPublisher;
-
-    @PostConstruct
-    public void init() {
-        System.out.println("INIT: " + this.getClass()
-                                          .getName());
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println("DESTROY: " + this.getClass()
-                                             .getName());
-    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
